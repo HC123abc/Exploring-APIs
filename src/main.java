fa7f0a9bdd6bdecf4ca3b89583201d3fb1c1567f
HC123abc <126921906+HC123abc@users.noreply.github.com>
2023-09-30 09:39:59 -0400

    added some in-line comment to explain code

--- src/main.java
+++ src/main.java
@@ -6,16 +6,19 @@ import java.net.URL;
 public class main {
 
     public static String request = "potato"; // user request, we will concatenate for now, however in future we will make sure to set reqeust parameter
+    // This is the URL for the API. It includes the user's request as a query parameter. So we can get specific recipes
     private static final String API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + request; // replace with your URL
 
     public static void main(String[] args) throws Exception {
         URL url = new URL(API_URL);
+        // create connection
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
+        // Set the request method to GET ie retrieve data
         conn.setRequestMethod("GET");
 
         //conn.setRequestProperty("s", "katsu");
 
+        // This allows us to read the info from the api call
         BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         String inputLine;
         StringBuffer content = new StringBuffer();
